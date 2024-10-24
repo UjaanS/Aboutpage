91b92d937d8d50ea4a4e0f3288f73e0ac7b3c350
UjaanS <155755341+UjaanS@users.noreply.github.com>
2024-10-25 01:19:21 +0530

    uppppp

--- AboutPage/src/App.jsx
+++ AboutPage/src/App.jsx
@@ -47,18 +47,24 @@ function Services() {
        <div className="about-container">
       <h1>About Us</h1>
       <p>
-        Welcome to our website! We are a group of students attending a hackathon for the first time ever! Though we lack a lot of experience and skill, we makeup for it with our effort. We feel strongly about the farmers of India.
+        Welcome to our website! We are a group of students attending a hackathon for the first time ever! Though we lack a lot of experience and skill, we makeup for it with our effort. We feel strongly about the farmers of India and their mental health. Thus we have come up with this platform to give them a one stop platform to help them out with their many struggles in life.
       </p>
       <p>
-        Our mission is to help people by offering various solutions tailored to their needs. We value quality, integrity, and customer satisfaction.
+        Our mission is to help farmers by educating them, helping them strategize their finances, and providing them with mental health support.
       </p>
       <h2></h2>
       <h1>Our Vision</h1>
-      <p>To become a leading platform in delivering exceptional value to our users.</p>
+      <p>To become a leading platform which greatly reduces the suicide rate of farmers in India.</p>
       <h2> </h2>
       <h1>Our Team</h1>
       <p>
-        We have a passionate team of professionals who work tirelessly to make sure our users get the best experience.
+        We have a passionate team of Students who work tirelessly to make this website usable:
+        <li>
+          <ul>Ujaan Sen</ul>
+          <ul>Aditya Thakur</ul>
+          <ul>Jiya</ul>
+          <ul>Simran Nayak</ul>
+        </li>
       </p>
       <br />
       <br />
